d8c82e0a7c97909f81513e39b2c4ce87a9038872
zqw10 <zqw1989@outlook.com>
2020-08-28 10:57:35 +0800

    add install script

---
+++ vim.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:nanofrog/vim.git ~/.vim
+cd ~/.vim
+mkdir bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/.vim/.vimrc ~/.vimrc
