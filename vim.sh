7147eeed63360cbf39c8e783b7fd731bdf790ce4
zqw10 <zqw1989@outlook.com>
2020-09-07 16:52:49 +0800

    clone vundle with depth 1

--- vim.sh
+++ vim.sh
@@ -1,5 +1,5 @@
 git clone git@github.com:nanofrog/vim.git ~/.vim
 cd ~/.vim
 mkdir bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
